d10c6f06af8486694ca5c86eed68a754f25503e8
joyyang0520 <yang10100520@gmail.com>
2023-04-19 23:31:54 +0800

    make it better

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template, request, make_response, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "楊子青測試網頁"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 if __name__ == "__main__":
     app.run()
